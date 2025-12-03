1ae67ac5dd4f29a228288078d259eaa9039f9240
Yusuf-Al-Spu <almagsusiy@spu.edu>
2025-12-03 06:21:53 -0800

    Formatting files

--- backend/app/auth/routes.py
+++ backend/app/auth/routes.py
@@ -1,1 +1,13 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+import jwt_handler as jwtFunc
+
+router = APIRouter()
+
+@router.post("/reigster")
+def register_user(data: dict):
+  return data
+
+@router.get("/login")
+def login_user(username: str, password: str):
+  jwtFunc.authenticate_user()
+  return {"username": username, "password": password}
