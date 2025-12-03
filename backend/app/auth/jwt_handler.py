434d041f6a1d5a02b33fda584d0bbd6972b2ef06
Yusuf-Al-Spu <almagsusiy@spu.edu>
2025-12-03 06:21:53 -0800

    Formatting files

--- backend/app/auth/jwt_handler.py
+++ backend/app/auth/jwt_handler.py
@@ -1,41 +1,14 @@
 import jwt
 from datetime import datetime, timedelta
-from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jwt.exceptions import InvalidTokenError
 from pwdlib import PasswordHash
-from pydantic import BaseModel
+import app.database
 
 SECRET_KEY = "29640ffbf5251fcafd1066268304879a62dcd61be7176245b563203901cec5c3"
 ALGORITHM = "HS256" 
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-fake_db = {
-  "user": {
-    "username": "fakeUser",
-    "full_name": "User Name",
-    "email": "username@gmail.com",
-    "hashed_password": "",
-    "disabled": False,
-    "role": "user"
-  }
-}
-
-class Token(BaseModel):
-   access_token: str
-   token_type: str
-
-class TokenData(BaseModel):
-   username: str | None = None
-
-class User(BaseModel):
-   username: str
-   email: str | None = None
-   full_name: str | None = None
-   disabled: bool | None = None
-
-class UserInDB(User):
-  hashed_password: str
 
 password_has = PasswordHash.recommended()
 
---
+++ backend/app/schemas/schema.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+class Token(BaseModel):
+   access_token: str
+   token_type: str
+
+class TokenData(BaseModel):
+   username: str 
+
+class User(BaseModel):
+   username: str
+   email: str 
+   full_name: str
+   role: str
+   disabled: bool
