5cb235acb9b8d6668fa56b303d170d4d87d1ed3e
Yusuf-Al-Spu <almagsusiy@spu.edu>
2025-12-03 00:29:05 -0800

    Partial JWT

---
+++ backend/app/database/database.py
@@ -0,0 +1,12 @@
+
+
+fake_db = {
+  "user": {
+    "username": "fakeUser",
+    "full_name": "User Name",
+    "email": "username@gmail.com",
+    "hashed_password": "",
+    "disabled": False,
+    "role": "user"
+  }
+}
