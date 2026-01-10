bff4e9da0876914446062eeaf37f41a1f8b242bc
briatran021094 <tranb10@spu.edu>
2026-01-09 20:07:54 -0800

    Database configuration

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -1,12 +1,8 @@
 from fastapi import APIRouter
 from .auth import router as Login_router
-from .Organization import router as Organization_router
-from .register import router as register_router
 
 
-router = APIRouter(prefix="/Evaide")
+router = APIRouter()
 
-router.include_router(Login_router)
-router.include_router(Organization_router)
-router.include_router(register_router)
+router.include_router(Login_router, prefix="/v1")
 
