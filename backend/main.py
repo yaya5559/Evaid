922659a84a8d9c6b92276e25151a60120dae1aa8
briatran021094 <tranb10@spu.edu>
2026-01-09 20:07:54 -0800

    Database configuration

--- backend/main.py
+++ backend/main.py
@@ -10,13 +10,13 @@ origins = [
     # Add more origins here in future (production URL, etc.)
 ]
 
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-app.include_router(api_router)
-
+app.include_router(api_router, prefix="/Evaide")
