f55d8c8247b21a5c4eb604e6c2b6529cbed0b5a4
Yusuf-Al-Spu <almagsusiy@spu.edu>
2026-02-06 02:41:03 -0800

    some add organization
    Added some of the backend "Add Organization."
    
    Removed "Register User" from loginServices.py and created a separate file for it
    
    Changed "UserName" from frontend to "First Name" and "Last Name"

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 from .auth import router as Login_router
+from .Organization import router as Organization_router
 
 
 router = APIRouter()
 
 router.include_router(Login_router)
+router.include_router(Organization_router)
 
