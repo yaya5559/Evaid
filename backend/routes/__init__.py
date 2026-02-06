794b71816e68453440edf8e23b6450cbd1cba788
Yusuf-Al-Spu <almagsusiy@spu.edu>
2026-02-06 02:52:25 -0800

    Making Files Modular
    Moved Register to its own route to make more modular and easier to understand

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
 from .auth import router as Login_router
 from .Organization import router as Organization_router
+from .register import router as register_router
 
 
 router = APIRouter()
 
 router.include_router(Login_router)
 router.include_router(Organization_router)
+router.include_router(register_router)
 
