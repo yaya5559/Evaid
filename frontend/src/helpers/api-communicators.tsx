4bc1a32fd9a7dec2c663c12e025e960d7e637096
yaya5559 <ouchcheny@spu.edu>
2026-02-10 18:40:15 -0800

    apiclient

--- frontend/src/helpers/api-communicators.tsx
+++ frontend/src/helpers/api-communicators.tsx
@@ -2,6 +2,16 @@ import axios from "axios"
 
 const path = '';
 
+
+type OrganizationT = {
+    companyName:string,
+    UserName: string,
+    email: string,
+    phone: string,
+    password: string,
+    
+}
+
 export const loginUser = async (email:string, password:string) => {
     try{
         const response = await axios.post(
@@ -16,4 +26,17 @@ export const loginUser = async (email:string, password:string) => {
     }catch(err){
 
     }
+}
+
+export const addOrganization = async (Organization: OrganizationT) => {
+    try{
+
+        const res = 
+
+    }catch(err: any){
+        const msg = err?.response?.data?.error || err?.response?.data?.message || err?.message || "Unable to Add Organization";
+        console.log("Register Organization error:", err?.response?.status, err?.response?.data || err);
+        throw new Error(msg);
+    }
+
 }
