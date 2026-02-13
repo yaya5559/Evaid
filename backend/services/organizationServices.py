47c6788a08023629df834e4e998ea1871926ebe5
Yusuf-Al-Spu <almagsusiy@spu.edu>
2026-02-13 02:05:33 -0800

    random changes
    small changes to allow for organization editing

--- backend/services/organizationServices.py
+++ backend/services/organizationServices.py
@@ -9,6 +9,24 @@ load_dotenv()
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto", bcrypt__truncate_error=False)
 
+def get_organization_id(name:str):
+
+  conn = get_db_connection()
+  cursor = conn.cursor()
+
+  try:
+    query = "SELECT org_id FROM organizations WHERE name = ? AND deleted_at IS NULL"
+    cursor.execute(query, (name,))
+    result = cursor.fetchone()
+
+    if result:
+            return result[0]  # Extract the org_id value
+    return None
+  
+  finally:
+    conn.close()
+
+
 def check_organization(name: str):
   conn = get_db_connection()
   cursor = conn.cursor()
@@ -48,6 +66,13 @@ def add_Organization(data: Organization):
     "VALUES (?, ?, ?, ?, 2, ?, 1)"
 
     cursor.execute(user_query, (data.first_name, data.last_name, data.email, hashed_password, org_id))
+    
+    cursor.execute("SELECT CAST(SCOPE_IDENTITY() AS INT)")
+    user_id = cursor.fetchone()[0]
+
+
+    update_query = "UPDATE organizations SET owner_id = ? WHERE org_id = ?"
+    cursor.execute(update_query, (user_id, org_id))
 
     conn.commit()
     return True
@@ -58,3 +83,43 @@ def add_Organization(data: Organization):
 
   finally:
     conn.close()
+
+def delete_organization(data:Organization):
+
+  conn = get_db_connection()
+  cursor = conn.cursor()
+
+  try:
+
+    delete_query = "DELETE FROM organizations WHERE name = ?"
+    cursor.execute(delete_query, (data.company_name,))
+    conn.commit()
+  
+    if cursor.rowcount == 0:
+      return False  # No rows deleted (not found)
+    
+    return True
+
+  except:
+    conn.rollback()
+    return False
+  
+  finally:
+    conn.close()
+
+def change_owner(new_owner: int, org_id: int):
+
+  conn = get_db_connection()
+  cursor = conn.cursor()
+
+  try: 
+
+    query = "UPDATE organizations SET owner_id = ? WHERE org_id = ?"
+    cursor.execute(query, (new_owner, org_id))
+    conn.commit()
+
+
+  finally:
+    conn.close()
+
+def change_name(new_name: str, org_id: int)
