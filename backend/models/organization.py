7fd25b0c34cc1dee83bc56d8498f97ce8b183926
Jemsoo <144981413+Jemsoo@users.noreply.github.com>
2026-03-02 14:46:05 -0800

    Add files via upload

--- backend/models/organization.py
+++ backend/models/organization.py
@@ -2,22 +2,8 @@ from pydantic import BaseModel, EmailStr
 
 class Organization(BaseModel):
     company_name: str
-    company_email: EmailStr
-    company_phone_number: str
-    owner_first_name: str
-    owner_last_name: str
-    owner_email: EmailStr
-    owner_phone_number: str
+    first_name: str
+    last_name: str
+    email: EmailStr
+    phone_number: str
     password: str
-
-class editedOrg(BaseModel):
-    org_id: int
-    companyName: str
-    companyEmail: EmailStr
-    companyPhoneNumber: str
-    ownerFirstName: str
-    ownerLastName: str
-    ownerEmail: str
-    ownerPhoneNumber: str
-    status: str
-    description: str
