0319662926be95604a8cedba1bc5aad9a7db1475
Jemsoo <144981413+Jemsoo@users.noreply.github.com>
2026-03-02 14:45:49 -0800

    Create register.py

--- backend/models/register.py
+++ backend/models/register.py
@@ -1,20 +1,8 @@
 from pydantic import BaseModel, EmailStr
-from typing import Optional
 
 class RegisterRequest(BaseModel):
     first_name: str
     last_name: str
     email: EmailStr
-    phone_number: str
     password: str
-    role_id: int = 3
     org_id: int = None # this is optional and can be assigned later
-
-class UsersUpdate(BaseModel):
-    user_id: int
-    first_name: Optional[str] = None
-    last_name: Optional[str] = None
-    email: Optional[EmailStr] = None
-    phone_number: Optional[str] = None
-    role_id: Optional[int] = None
-    org_id: Optional[int] = None
