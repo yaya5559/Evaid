160c16353b9f413adb38926007c1c0324d9c1c96
Yusuf-Al-Spu <almagsusiy@spu.edu>
2026-03-17 14:01:42 -0700

    Routes for case management
    Added all the routes for the case management, adding things for agents, org admins, and evaide admins.

--- backend/routes/org_admin/case_org_admin copy.py
+++
@@ -1,9 +0,0 @@
-from fastapi import HTTPException, status, APIRouter
-from models.cases import Case
-import backend.services.evaide_admin.admin_case_services as services
-
-router = APIRouter(prefix="/Cases", tags=["Cases"])
-
-@router.get("/list_all_cases")
-def all_cases():
-  return services.list_all_cases()
