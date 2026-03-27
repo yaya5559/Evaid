80f9eef83e6ba81d5de7d9cbb26b003c6d7f52a4
Yusuf-Al-Spu <almagsusiy@spu.edu>
2026-03-26 23:32:01 -0700

    Case redesign
    Redesigned the cases layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,9 @@ import OrgDashboard from './components/organization/OrgDashboard'
 import OrgCaseProgress from './components/organization/OrgCaseProgress'
 import AddAgent from './components/admin/AddAgents'
 import Cases from './components/admin/Cases'
+import AdminCaseDetail from './components/admin/CaseDetail'
+import OrgCaseDetail from './components/organization/OrgCaseDetail'
+import AgentCaseDetail from './components/agent/AgentCaseDetail'
 import EvidenceUpload from './components/Evidence/EvidenceUpload'
 import AgentCases from './components/agent/AgentCases'
 
@@ -43,13 +46,16 @@ function App() {
       <Route path="/Edit_Organization" element={<ProtectedRoute allowedRoles={['evaide_admin']}><EditOrganization /></ProtectedRoute>} />
       <Route path="/Register_Agent" element={<ProtectedRoute allowedRoles={['evaide_admin']}><AddAgent /></ProtectedRoute>} />
       <Route path="/Cases" element={<ProtectedRoute allowedRoles={['evaide_admin']}><Cases /></ProtectedRoute>} />
+      <Route path="/Cases/:orgId/:caseId" element={<ProtectedRoute allowedRoles={['evaide_admin']}><AdminCaseDetail /></ProtectedRoute>} />
 
       {/* Org Admin only */}
       <Route path="/Org_Dashboard" element={<ProtectedRoute allowedRoles={['org_admin']}><OrgDashboard /></ProtectedRoute>} />
       <Route path="/OrgCaseProgress" element={<ProtectedRoute allowedRoles={['org_admin']}><OrgCaseProgress /></ProtectedRoute>} />
+      <Route path="/OrgCase/:caseId" element={<ProtectedRoute allowedRoles={['org_admin']}><OrgCaseDetail /></ProtectedRoute>} />
 
       {/* Agent only */}
       <Route path="/AgentCases" element={<ProtectedRoute allowedRoles={['agent']}><AgentCases /></ProtectedRoute>} />
+      <Route path="/AgentCase/:caseId" element={<ProtectedRoute allowedRoles={['agent']}><AgentCaseDetail /></ProtectedRoute>} />
 
       {/* Any authenticated user */}
       <Route path="/Evidence_Upload" element={<ProtectedRoute allowedRoles={['evaide_admin', 'org_admin', 'agent']}><EvidenceUpload /></ProtectedRoute>} />
